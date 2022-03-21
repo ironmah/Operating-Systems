3447cf236db669e128475db44c0c386adc23fc20
Mahmud <91074830+loshadkaigogo@users.noreply.github.com>
2022-03-21 12:27:32 +0300

    Add files via upload

---
+++ os_lab5/src/task1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+extern void swap(int*, int*);
+extern int PrimeCount(int, int);
+extern int GCF(int, int);
+
+int main()
+{
+	int ind, A, B, result;
+	while ((scanf("%d", &ind)) != EOF)
+	{
+		if ((scanf ("%d %d", &A, &B)) != EOF)
+		{
+			if (ind == 1)
+			{
+				result = PrimeCount(A, B);
+				if (result == -1)
+				{
+					printf("At least A or B must be non-negative numbers\n");
+				}
+				else
+				{
+					printf("%d\n", result);
+				}
+			}
+			else if (ind == 2)
+			{
+				result = GCF (A, B);
+				if (result == -1)
+				{
+					printf("Infinity\n");
+				}
+				else
+				{
+				        printf("%d\n", result);
+				}
+			}
+			else
+			{
+				printf("First digit must be 1 or 2\n");
+			}
+		}
+	}
+	return 0;
+}
